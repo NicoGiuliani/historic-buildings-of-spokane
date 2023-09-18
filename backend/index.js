4bcd931ab83fff4851a558bf78a64ab3b91a9c5a
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-09-18 13:05:17 -0700

    Started on building profile page

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,50 @@
 import express from "express";
-import { Building } from "./models/buildingModel.js";
+import { PORT } from './config.js';
 import path from 'path';
 import pool from "./db/connection.js";
 import * as url from 'url';
-import 'dotenv/config';
 
 const app = express();
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.set("views", path.join(__dirname, 'views'));
 app.set("view engine", "pug");
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(PORT, () => {
+  console.log(`App is listening on port: ${PORT}`);
+});
+
+app.get('/profile/:buildingName', async (request, response) => {
+  const buildingName = request.params['buildingName'];
+  console.log(buildingName);
+  try {
+    pool.query('SELECT * FROM buildings WHERE buildings.name = ?', [buildingName], (error, result) => {
+      if (error) {
+        console.log(error);
+      }
+      response.render("profile", { building: result[0] });
+    });
+  } catch(error) {
+    console.log(error);
+  }
+});
+
+// app.get('/test', async (request, response) => {
+//   try {
+//     pool.query('SELECT * FROM buildings', (error, result) => {
+//       if (error) {
+//         console.log(error);
+//       }
+//       console.log(result[0]);
+//       response.render("test", { thing: result[0] });
+//     });
+//   } catch(error) {
+//     response.render("error");
+//   }
+// });
+  
 app.get('/buildings', async (request, response) => {
   try {
     pool.query('SELECT * FROM buildings', (error, result) => {
@@ -36,13 +68,11 @@ app.post('/buildings', async (request, response) => {
         message: "Send all required fields"
       });
     } 
-    const newBuilding = {
+    const building = {
       name: request.body.name,
       year_built: request.body.year_built,
-      year_destroyed: request.body.year_destroyed,
-      resources: request.body.resources,
+      year_destroyed: request.body.year_destroyed || null,
     }
-    const building = await Building.create(newBuilding);
     pool.query('INSERT INTO buildings (name, year_built, year_destroyed) VALUES (?, ?, ?)', 
     [building.name, building.year_built, building.year_destroyed], 
     (error, result) => {
@@ -58,13 +88,31 @@ app.post('/buildings', async (request, response) => {
   }
 });
 
-// mongoose.connect(mongoDBURL)
-// .then(() => {
-//   console.log("Connected to the database");
-//   app.listen(PORT, () => {
-//     console.log(`App is listening on port: ${PORT}`);
-//   });
-// })
-// .catch((error) => {
-//   console.log(error);
-// });
+app.post('/resources', async (request, response) => {
+  try {
+    if (
+      !request.body.building ||
+      !request.body.url
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields"
+      });
+    } 
+    const resource = {
+      building: request.body.building,
+      url: request.body.url,
+    }
+    pool.query('INSERT INTO resources (building, url) VALUES (?, ?)', 
+    [resource.building, resource.url], 
+    (error, result) => {
+      if (error) {
+        return response.status(500).send({ message: error.message });
+      } else {
+        return response.status(201).send(resource);
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+    return response.status(500).send({ message: error.message });
+  }
+});
